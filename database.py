8800ffc509f71e89ce5a5129d28a3ecd7968573f
Kristina Creek <144278926+KCreek1@users.noreply.github.com>
2024-12-20 11:45:40 -0700

    remove  env for production

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
 # database configuration - separate file per duck suggestion
 import os
 
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy 
 
-load_dotenv(dotenv_path='./.env')
+#load_dotenv(dotenv_path='./.env')
 database_url = os.getenv('DATABASE_URL')
 
 # print(f"Database URL: {database_url}")
